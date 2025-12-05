e336a7582a1b00da2fed96e2a096776e8e19afde
adam <adam.sinc124@gmail.com>
2025-12-05 13:32:31 +0000

    feat: sort point by centroid into bins

--- main.py
+++ main.py
@@ -16,20 +16,26 @@ def random_centroids(data_array, k):
     # k          = number of random centroids
     # data_array = the data being used
 
-    centroids = set()
-    for i in range(k):
+    centroids = []
+    for _ in range(k):
         unique = False
         while not unique:
-            random_x = random.randint(0, len(data_array[0]))
-            random_y = random.randint(0, len(data_array))
-            point = [random_x, random_y]
+            random_x = random.randint(0, len(data_array[0]) - 1)
+            random_y = random.randint(0, len(data_array) - 1)
+            point = (random_x, random_y)
             if point not in centroids:
                 unique = True
-                centroids += point
+                centroids.append(point)
+
+    return centroids
 
 
 def bins_setup(centroids):
-    for i in range(len(centroids)):
+    bins = {}
+    for centroid in centroids:
+        bins[centroid] = []
+
+    return bins
 
 
 def main():
@@ -37,16 +43,26 @@ def main():
                  [0, 0, 1, 0, 0],
                  [0, 0, 0, 0, 1],
                  [0, 1, 0, 0, 0],
-                 [0, 0, 0, 1, 0]],
+                 [0, 0, 0, 1, 0]]
+    points = [(0, 4), (1, 1), (2, 3), (3, 0), (4, 2)]
 
     k = 3
     centroids = random_centroids(data_array=temp_data, k=k)
     bins = bins_setup(centroids)
+
     # Calculate euclidean distance between each data object (o) and each centroid (c)
     # 1. Go through each data point and calculate the eucl_distance between it and each cluster
     # 2. Put them in bins for each cluster that it's closest to
+    for point in points:
+        shortest_distance = 100_000_000  # Picked to represent infinity
+        chosen_centroid = None
+        for centroid in centroids:
+            distance = eucl_distance(point, centroid)
+            if distance < shortest_distance and distance >= 0:
+                shortest_distance = distance
+                chosen_centroid = centroid
 
-    # Cluster each data point with the cluster it is closest to
+        bins[chosen_centroid].append(point)
 
 
 if __name__ == "__main__":
