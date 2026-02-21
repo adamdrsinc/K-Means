8dc9bed50be888bb547f257f52a9cc027e3e0b90
adamdrsinc <adam.sinc124@gmail.com>
2026-02-21 14:35:24 +0000

    Rename variables for readability, remove unused variables

--- GraphPlotting.py
+++ GraphPlotting.py
@@ -1,33 +1,34 @@
-import NDKMeans
+from NDKMeans import NDKMeans
 import matplotlib.pyplot as plot
 
+
 def plot_elbow(data, title):
-    inertia_scores = {}
-    inertias = []
-    ks = []
+    inertiaScores = []
+    kValues = []
+
+    # Trying different K values from 2-10.
     for i in range(2, 10):
         ndkMeans = NDKMeans(data, i, 300)
         ndkMeans.perform_kmeans()
 
         inertia = ndkMeans.inertia()
+        inertiaScores.append(inertia)
 
-        inertia_scores[i] = inertia
-        inertias.append(inertia)
+        kValues.append(i)
 
-        ks.append(i)
-
-    plot.plot(ks, inertias)
+    plot.plot(kValues, inertiaScores)
     plot.title(title)
     plot.ylabel("Inertia")
     plot.xlabel("K Value")
     plot.show()
+
 
 def plot_cardinality(ndkmeans):
     centroids = [i for i in range(0, len(ndkmeans.centroids))]
     points = []
     for _, values in ndkmeans.bins.items():
         points.append(len(values))
-    
+
     plot.bar(centroids, points)
     plot.xlabel("Centroid")
     plot.ylabel("Number of Items")
